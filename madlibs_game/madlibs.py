4176c3aba21557383ca92c88dc16c12ffc60dea7
fabulousugo <fabulousonah@gmIL.COM>
2023-09-15 18:16:42 +0100

    Initial

---
+++ madlibs_game/madlibs.py
@@ -0,0 +1,25 @@
+"""
+**Title:** **The Great Adventure**
+
+**Instructions:** 
+1. Ask a friend for the following words:
+   - Noun
+   - Verb (past tense)
+   - Adjective
+   - Place
+   - Adverb
+   - Number
+   - Animal
+   - Exclamation
+
+2. Fill in the blanks with the words your friend gives you.
+
+**Story (with blanks):**
+Once upon a time, there was a [Noun] who [Verb (past tense)] in a [Adjective] [Place]. One [Adverb] day, they found [Number] [Animal]s [Verb] in the [Place]. The [Animal]s all shouted, "[Exclamation]!"
+
+**Filled-in Story:**
+Once upon a time, there was a **cat** who **danced** in a **sparkling** **forest**. One **bright** day, they found **seven** **elephants** **singing** in the **moonlight**. The **elephants** all shouted, "**Wow**!"
+
+When you read the filled-in story aloud, it often results in a humorous and entertaining narrative because the chosen words might not fit the original context, leading to unexpected and funny combinations. That's the essence of the Mad Libs game.
+"""
+
